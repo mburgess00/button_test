d1c91c5c1a16aafa0b5a4fb05fb67c0408d8083d
Matt Burgess <mburgess@celestra.digitalglobe.com>
2018-07-26 14:31:18 -0600

    testing physical buttons

--- button_test.ino
+++ button_test.ino
@@ -32,194 +32,45 @@ byte buzzer = 8;
 byte resetButton = 9;
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-unsigned long previousMillis = 0;
-boolean timerRunning = false;
-boolean currentGame = false;
-int timer = 30;
-int score = 0;
-int highscore = 0;
-int currentButton = 0; //none=0, A=1, B=2, C=3, D=4, reset=-1
-int previousButton =0;
-unsigned long presstime = 0;
 
 const int eeAddress = 0;
-
-boolean noDecimals[4] = {false, false, false, false};
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Putter Scoreboard");
-
-  pinMode(segmentClock, OUTPUT);
-  pinMode(segmentData, OUTPUT);
-  pinMode(segmentLatch, OUTPUT);
-
-  digitalWrite(segmentClock, LOW);
-  digitalWrite(segmentData, LOW);
-  digitalWrite(segmentLatch, LOW);
+  Serial.println("Button Test!!!");
 
   //remote receiver
-  pinMode(aPin, INPUT);
-  pinMode(bPin, INPUT);
-  pinMode(cPin, INPUT);
-  pinMode(dPin, INPUT);
+  pinMode(aPin, INPUT_PULLUP);
+  pinMode(bPin, INPUT_PULLUP);
+  pinMode(cPin, INPUT_PULLUP);
+  pinMode(dPin, INPUT_PULLUP);
 
-  //buzzer
-  pinMode(buzzer, OUTPUT);
-  //digitalWrite(buzzer, HIGH);
-  //delay(250);
-  //digitalWrite(buzzer, LOW);
-  tone(buzzer, 1200, 250);
-
-  //reset
-  pinMode(resetButton, INPUT_PULLUP);
-
-  //read highscore from eeprom
-  EEPROM.get(eeAddress, highscore);
-  if ((highscore > 200) || (highscore < 0))
-  {
-    highscore = 0;
-  }
-
-  sendString(' ', ' ', ' ', ' ', noDecimals);
 }
 
 
 void loop()
 {
   //handle buttons
-  /*if (digitalRead(aPin) == HIGH)
+  if (digitalRead(aPin) == LOW)
   {
-    postNumber('a', false);
+    Serial.println("A is being pressed");
   }
-  if (digitalRead(bPin) == HIGH)
+  if (digitalRead(bPin) == LOW)
   {
-    postNumber('b', false);
+    Serial.println("B is being pressed");
   }
-  if (digitalRead(cPin) == HIGH)
+  if (digitalRead(cPin) == LOW)
   {
-    postNumber('c', false);
+    Serial.println("C is being pressed");
   }
-  if (digitalRead(dPin) == HIGH)
+  if (digitalRead(dPin) == LOW)
   {
-    postNumber('d', false);
+    Serial.println("D is being pressed");
   }
-  delay(250);*/
 
-
-  int remainder = timer % 10;
-  char timer1 = char(remainder);
-  int temptimer = timer / 10;
-  char timer2 = char(temptimer);
-
-
-  if (digitalRead(aPin) == HIGH)
-  {
-    Serial.println("got the a pin!");
-    sendString(' ', 3, timer2, timer1, noDecimals);
-    tone(buzzer, 1000, 250);
-    delay(750);
-    sendString(' ', 2, timer2, timer1, noDecimals);
-    tone(buzzer, 1000, 250);
-    delay(750);
-    sendString(' ', 1, timer2, timer1, noDecimals);
-    tone(buzzer, 1000, 250);
-    delay(750);
-    sendString('g', 'o', timer2, timer1, noDecimals);
-    tone(buzzer, 1200, 1000);
-  
-
-    while (timer > 0)
-    {
-      delay(1000);
-      Serial.print("timer=");
-      Serial.println(timer);
-      timer--;
-      remainder = timer % 10;
-      timer1 = char(remainder);
-      temptimer = timer / 10;
-      timer2 = char(temptimer);
-      sendString(' ', ' ', timer2, timer1, noDecimals);
-    }
-  }
+  delay(100);
   
 }
 
 
-void sendString(byte a, byte b, byte c, byte d, boolean decimals[4])
-{
-  Serial.print("Sending ");
-  Serial.println(a + b + c + d);
-  postChar(d, decimals[3]);
-  postChar(c, decimals[2]);
-  postChar(b, decimals[1]);
-  postChar(a, decimals[0]);
-  digitalWrite(segmentLatch, LOW);
-  digitalWrite(segmentLatch, HIGH);  
-}
-
-//Given a number, or '-', shifts it out to the display
-void postChar(byte digit, boolean decimal)
-{
-  //    -  A
-  //   / / F/B
-  //    -  G
-  //   / / E/C
-  //    -. D/DP
-
-#define a  1<<0
-#define b  1<<6
-#define c  1<<5
-#define d  1<<4
-#define e  1<<3
-#define f  1<<1
-#define g  1<<2
-#define dp 1<<7
-
-  //Serial.print("sending the following character: ");
-  //Serial.println(digit);
-
-  byte segments;
-  Serial.print("digit: ");
-  Serial.println(digit);
-
-  switch (digit)
-  {
-    case 1: segments = b | c; break;
-    case 2: segments = a | b | d | e | g; break;
-    case 3: segments = a | b | c | d | g; break;
-    case 4: segments = f | g | b | c; break;
-    case 5: segments = a | f | g | c | d; break;
-    case 6: segments = a | f | g | e | c | d; break;
-    case 7: segments = a | b | c; break;
-    case 8: segments = a | b | c | d | e | f | g; break;
-    case 9: segments = a | b | c | d | f | g; break;
-    case 0: segments = a | b | c | d | e | f; break;
-    case ' ': segments = 0; break;
-    case 'a': segments = a | b | c | e | f | g; break;
-    case 'b': segments = c | d | e | f | g; break;
-    case 'c': segments = g | e | d; break;
-    case 'd': segments = b | c | d | e | g; break;
-    case 'e': segments = a | d | e | f | g; break;
-    case 'f': segments = a | e | f | g; break;
-    case 'g': segments = a | b | c | d | f | g; break;
-    case 'h': segments = c | e | f | g; break;
-    case 'o': segments = a | b | f | g; break;
-    case '-': segments = g; break;
-    default: segments = 0;
-  }
-
-  if (decimal) segments |= dp;
-  Serial.print("segments: ");
-  Serial.println(segments);
-
-  //Clock these bits out to the drivers
-  for (byte x = 0 ; x < 8 ; x++)
-  {
-    digitalWrite(segmentClock, LOW);
-    digitalWrite(segmentData, segments & 1 << (7 - x));
-    digitalWrite(segmentClock, HIGH); //Data transfers to the register on the rising edge of SRCK
-  }
-
-}
